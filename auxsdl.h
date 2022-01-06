0a537bc06fc0d13bf505bb2ed42fc14561ab658c
Ramon Daniel Regueiro-Espino <93584869+RD-RegueiroEspino@users.noreply.github.com>
2022-01-06 13:01:24 +0100

    Add files via upload

--- auxsdl.h
+++ auxsdl.h
@@ -9,12 +9,24 @@
 //#include <SDL2/SDL.h>
 //#include <SDL2/SDL_image.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 class auxsdl{
 private:
-    int donnes_;
+    int width_;
+    int heigth_;
 public:
     auxsdl(int i){donnes_=i;}
     
 };
+
+SDL_Window ∗win = SDL_CreateWindow ("Bitmap",
+SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                    width ,heigth,SDL_WINDOW_OPENGL);
+SDL_Renderer ∗ren = SDL_CreateRenderer(win,−1,0);
+SDL_SetRenderDrawColor (ren,0,0,0,SDL_ALPHA_OPAQUE);
+//figure
+SDL_RenderClear(ren);
+SDL_RenderPresent (ren);
+SDL_Quit ( );
 
 #endif /* auxsdl_h */
