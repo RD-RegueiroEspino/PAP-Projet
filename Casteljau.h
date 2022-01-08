f389078113399b4c6f83d97a2efe2770878cf709
Ramon Daniel Regueiro-Espino <93584869+RD-RegueiroEspino@users.noreply.github.com>
2022-01-08 19:35:49 +0100

    Add files via upload

--- Casteljau.h
+++ Casteljau.h
@@ -11,20 +11,49 @@
  */
 class Casteljau: public R2{
 protected:
-    int size_;
-    R2 *data_;
+    int size_;  /*!< Taille du objet*/
+    R2 *data_;  /*!< Objet d'éléments R2*/
 public:
+    /*!
+     *\fn Casteljau(int size)
+     * \brief {Constructeur}
+     * \param[in] size Premier élément
+     */
     Casteljau(int size);
+    /*!
+     *\fn ~Casteljau()
+     * \brief {Destructeur}
+     */
     ~Casteljau();
+    /*!
+     *\fn Casteljau(const Casteljau & v)
+     * \brief {Constructeur de copie}
+     * \param[in] v Élément Casteljau à copier
+     */
     Casteljau(const Casteljau & v);
-    
+    /*!
+     *\fn R2& operator[](int l)
+     * \brief {Écriture d'un élément du objet}
+     * \param[in] l Position du élément dans l'objet
+     */
     R2& operator[](int l){return data_[l];}
+    /*!
+     *\fn R2 operator[](int l)
+     * \brief {Lecture de chaque élément du objet}
+     * \param[in] l Position du élément dans l'objet
+     */
     R2 operator[](int l)const {return data_[l];}
-    
+    /*!
+     *\fn int get_size()const
+     * \brief {Obtention de la taille du objet}
+     */
     int get_size()const{return size_;}
 };
 /*!
- * \brief {Calcul du valeur de la courbe de Bezier en t}
+ *\fn R2 pointcourbe(Casteljau & T, const double t)
+ * \brief {Calcul du point t de la courbe}
+ * \param[in] T Objet de la classe Casteljau
+ * \param[in] t Valeur de [0,1] pour lequelle on veut connaître le point de la courbe
  */
 R2 pointcourbe(Casteljau & T, const double t);
 #endif /* Casteljau_h */
