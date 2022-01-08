b77d1e9908f9f92cc1202cfab0be14c9031b6328
Ramon Daniel Regueiro-Espino <93584869+RD-RegueiroEspino@users.noreply.github.com>
2022-01-08 19:41:35 +0100

    Add files via upload

--- Remplisage.h
+++ Remplisage.h
@@ -12,20 +12,50 @@
  */
 class Remplisage{
 private:
-    int size_;
-    R2 *donnes_;
+    int size_;/*!< Taille du objet*/
+    R2 *donnes_;/*!< Objet d'éléments R2*/
 public:
+    /*!
+     *\fn Remplisage(int size)
+     * \brief {Constructeur}
+     * \param[in] size Taille du objet à créer
+     */
     Remplisage(int size);
+    /*!
+     *\fn ~Remplisage()
+     * \brief {Destructeur}
+     */
     ~Remplisage();
+    /*!
+     *\fn Remplisage(const Remplisage& copy)
+     * \brief {Constructeur de copie}
+     * \param[in] copy Objet à copier
+     */
     Remplisage(const Remplisage& copy);
-    
+    /*!
+     *\fn R2& operator[](int l)const
+     * \brief {Écriture d'un élément du objet}
+     * \param[in] l Position du élément dans l'objet
+     */
     R2& operator[](int l){return donnes_[l];}
+    /*!
+     *\fn R2 operator[](int l)const
+     * \brief {Lecture de chaque élément du objet}
+     * \param[in] l Position du élément dans l'objet
+     */
     R2 operator[](int l)const {return donnes_[l];}
-    
+    /*!
+     *\fn int get_size()const
+     * \brief {Obtention de la taille du objet}
+     */
     int get_size()const{return size_;}
 };
 /*!
- * \brief {Calcul de l'ensemble des points considérés à partir des points initials}
+ *\fn void Calcul(Casteljau E, Remplisage & T)
+ * \brief {Calcul des valeurs dans la courbe des points du objet}
+ * \param[in] E Points de côntrol de la courbe
+ * \param[in] T Points à calculer
+
  */
 void Calcul(Casteljau E, Remplisage & T);
 #endif /* Remplisage_h */
