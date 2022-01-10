260d414bcae8f9e9b81d020f26687f8f4febb8f3
Ramon Daniel Regueiro-Espino <93584869+RD-RegueiroEspino@users.noreply.github.com>
2022-01-10 10:00:58 +0100

    Update AffichageSDL.h

--- AffichageSDL.h
+++ AffichageSDL.h
@@ -21,44 +21,31 @@ class AffichageSDL{
         * \param[in] heigth Hauteur du bitmap
         */
         void Dessine(int x, int width, int heigth) {
-            
             /*Sélection du Bitmap à dessiner dans la fenêtre SDL*/
             std::cout<<"Bitmap of the chosen letter:" <<std::endl;
             Bitmap table(width,heigth);
             selection(table,x);
             std::cout<<"Please wait few seconds, display with SDL in progress..."<<std::endl;
-
             /* Gestion de l'erreur de l'initialisation de la SDL */
             if (SDL_Init(SDL_INIT_VIDEO) != 0 ) {
                 std::cout << "Echec de l'initialisation" << std::endl;;
-            
             }
-
             /*Création de la fenêtre SDL*/
             SDL_Window* Fenetre = NULL;
             Fenetre = SDL_CreateWindow("Projet",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, table.get_width()*20.5, table.get_heigth()*7, 0);
-            
             /*Gestion de l'erreur de création de fenêtre*/
             if (!Fenetre){
                 std::cout << "Erreur de création de la fenêtre: %s" << std::endl;
             }
-
             /*création du fond blanc de la fenêtre SDL*/
             SDL_Renderer* rendu;
             rendu = SDL_CreateRenderer(Fenetre, -1, SDL_RENDERER_ACCELERATED);
             SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
             SDL_RenderClear(rendu);
-     
-
-
             /*création de rectangles pleins (pixels) afin de dessiner la lettre du bitmap*/
-
             int** bitmap_lettre = table.get_table();
             for(int i=0; i<table.get_heigth(); i++){
-            
-            
                 for(int j=0; j<table.get_width(); j++){
-                
                     /*Dessine lettre contour noir seul*/
                     if(bitmap_lettre[j][i]==2 ){ 
                         SDL_Rect srcrect; 
@@ -68,9 +55,7 @@ class AffichageSDL{
                         srcrect.h = 5;
                         SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
                         SDL_RenderFillRect(rendu, &srcrect);
-
                     } 
-
                     /* Dessine lettre noir pleine*/
                     if( bitmap_lettre[j][i]==1 ){ 
                         SDL_Rect srcrect; 
@@ -80,9 +65,7 @@ class AffichageSDL{
                         srcrect.h = 5;
                         SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
                         SDL_RenderFillRect(rendu, &srcrect);
-
                     } 
-
                     /*Dessine lettre intérieur noir */
                     if(bitmap_lettre[j][i]==1){ 
                         SDL_Rect srcrect; 
@@ -93,7 +76,6 @@ class AffichageSDL{
                         SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
                         SDL_RenderFillRect(rendu, &srcrect);
                     }
-
                     /*Dessine le contour de lettre avec intérieur en noir */
                     if(bitmap_lettre[j][i]==2){ 
                         SDL_Rect srcrect; 
@@ -106,10 +88,8 @@ class AffichageSDL{
                     }   
                 }
             }
-
             /*Affichage de la fenêtre*/
             SDL_RenderPresent(rendu);
-
             /*Boucle permettant de bien quitter le programme lorsque l'on ferme la fenêtre*/
             int continu = 1;
             while (continu) {
@@ -121,11 +101,8 @@ class AffichageSDL{
                         break;
                     }
                 }
-        
             }
         SDL_Quit();
-    
-
         }
 };
 
